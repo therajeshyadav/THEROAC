84e035963933fdcba6f78d096f9727e1c43d9aa5
sharan036 <sharangopal36@gmail.com>
2025-10-27 17:32:22 +0530

    updated code

--- theroac/src/pages/AdminDashboard.jsx
+++ theroac/src/pages/AdminDashboard.jsx
@@ -93,6 +93,21 @@ const AdminDashboard = () => {
 
     return (
         <div className="dashboard-container">
+            <div className="preloader">
+                <div className="loading-container">
+                <div className="loading"></div>
+                <div id="loading-icon">
+                    <img src="assets/img/logo/preloader.png" alt="" />
+                </div>
+                </div>
+            </div>
+            <div className="paginacontainer">
+                <div className="progress-wrap warp2">
+                <svg className="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+                    <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+                </svg>
+                </div>
+            </div> 
             <div className="dashboard-background"></div>
 
             {/* Dashboard Header with Navigation */}
--- theroac/src/pages/Jobs.jsx
+++ theroac/src/pages/Jobs.jsx
@@ -2,6 +2,22 @@ import React from 'react';
 
 const Jobs = () => {
   return (
+    <>
+    <div className="preloader">
+      <div className="loading-container">
+        <div className="loading"></div>
+        <div id="loading-icon">
+          <img src="assets/img/logo/preloader.png" alt="" />
+        </div>
+      </div>
+    </div>
+    <div className="paginacontainer">
+      <div className="progress-wrap warp2">
+        <svg className="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+          <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+        </svg>
+      </div>
+    </div> 
     <div className="jobs-page">
       <div className="container">
         <div className="row">
@@ -84,6 +100,7 @@ const Jobs = () => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 

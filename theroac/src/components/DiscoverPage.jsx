1126d6d496f9a0bed030689c794b5933b0228458
Rajesh Yadav <ry8036739@gmail.com>
2025-12-26 00:40:41 +0530

    updated css

--- theroac/src/components/DiscoverPage.jsx
+++ theroac/src/components/DiscoverPage.jsx
@@ -239,13 +239,13 @@ const DiscoverPage = () => {
     return (
       <>
         {/* Progress Wrap - Scroll to Top */}
-        <div className="paginacontainer">
+        {/* <div className="paginacontainer">
           <div className="progress-wrap warp2">
             <svg className="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
               <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
             </svg>
           </div>
-        </div>
+        </div> */}
 
         <div className="discover-page-wrapper">
           <div className="discover-page">
@@ -340,8 +340,15 @@ const DiscoverPage = () => {
                 )}
               </div>
             ) : (
-              <div className="discover-event-slider-wrapper">
-                <EventSlider eventsData={discoverData} />
+              <div className="event10-section-area sp3">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-lg-12">
+                      <EventSlider eventsData={discoverData} />
+                      <div className="space30"></div>
+                    </div>
+                  </div>
+                </div>
               </div>
             )}
           </div>
